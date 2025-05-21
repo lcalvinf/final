d8b2261e16971af62d3b4f3830c98af05aaaeb22
lcalvinf <l.calvin.foster@gmail.com>
2025-05-21 18:43:16 -0400

    Change the colors a bit

--- colors.py
+++ colors.py
@@ -1,17 +1,17 @@
 import pygame as pg
 
-WHITE = pg.Color(255,255,255)
-BLACK = pg.Color(0,0,0)
-RED = pg.Color(255,0,0)
-GREEN = pg.Color(0,255,0)
-BLUE = pg.Color(0,0,255)
+WHITE = pg.Color(240,255,240)
+BLACK = pg.Color(20,40,20)
+RED = pg.Color(220,50,50)
+GREEN = pg.Color(50,200,70)
+BLUE = pg.Color(40,20,200)
 
 COLORS = {
     "player": WHITE,
     "ball": RED,
     "red-ball": RED,
     "blue-ball": BLUE,
-    "background": pg.Color(50,200,50),
+    "background": GREEN,
     "foreground": BLACK,
     "hole": BLACK,
     "markers": WHITE,
